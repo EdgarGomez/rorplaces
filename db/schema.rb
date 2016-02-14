6c04ea6e2697a0c9181258f89b1ab12d311f7883
edgar gomez <edgarjgp@gmail.com>
2016-02-14 13:23:46 +0100

    Added reviews to places

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160213185540) do
+ActiveRecord::Schema.define(version: 20160214120228) do
 
   create_table "places", force: :cascade do |t|
     t.string   "name"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20160213185540) do
     t.datetime "updated_at",  null: false
     t.float    "longitude"
     t.float    "latitude"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "place_id"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
